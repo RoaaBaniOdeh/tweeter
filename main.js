06600aa6e33ec7a7e246cbd4ee2b1ea90e6df48b
RoaaBaniOdeh <rooa.mohye@gmail.com>
2022-07-21 13:21:14 -0700

    tweeter

--- main.js
+++ main.js
@@ -14,17 +14,28 @@ renderer.renderPostsk(tweeter.getPostsK())
 }
 
 const deletePost =function (){
-     let postId = $(this).closest(".posts").find(".post").data().id
+    // let postId = $(this).closest(".posts").find(".post").data().id
+    let postId = $(this).closest(".post").data().id
      console.log(postId)
     tweeter.removePostK(postId) 
     renderer.renderPostsk(tweeter.getPostsK())
 }
 
-const comment = function () {
+/*const comment = function () {
   const value =  $(this).find("#input-comment").val();//$("#input-comment").val()
    tweeter.addCommentk(value) 
    renderer.renderPostsk(tweeter.getPostsK())
+}*/
+
+const comment = function () {
+	let postId=$(this).closest('div').data().id
+	console.log($(this).closest('div').data().id)
+  const value =  $(this).closest('div').find('input').val();//$("#input-comment").val()
+   tweeter.addCommentk(postId,value)
+   renderer.renderPostsk(tweeter.getPostsK())
 }
-$("body").on("click", "#post",post());
+$("body").on("click", "#post",post);
 $("body").on("click", ".delete",deletePost());
 $("body").on("click", ".comments",comment());
+
+
--- render.js
+++ render.js
@@ -1,60 +1,27 @@
 const Renderer = function(){
 
+  const renderPosts = function(posts){
+    $(".posts").empty();
+    let postBox
+    let comm
+    for(let post of posts)
+     {
+         $(".posts").append($(`<div class="post"  id =${post.id} data-id=${post.id}>  <span class="delete" >x</span> ${post.text}</div>`))
+         for (let comment of post.comments){
+  $(`#${post.id}`).append($(`<div id=${comment.id}> </div>`))
+  $(`#${comment.id}`).append($(`<span class="delete-comment" > x </span>`))
+  $(`#${comment.id}`).append($(`<span class="comments" data-id=${comment.id}> ${comment.text}</span>`))
+}
+$(`#${post.id}`).append($(`<div>  </div>`))
+$(`#${post.id}`).append($(`<input type text class="#input-comment" placeholder="got something to say? ">  </input>`))
+$(`#${post.id}`).append($(`<button type text class="comment"  ">Comment</button>`))
+$(`#${post.id}`).append($(`<div>  </div>`))
+}
+}
 
-
-    const renderPosts = function(posts){
-       
-        $(".posts").empty();
-        let postBox
-        let comm
-        for(let post of posts)
-         {
-
-
-
-          for(let comment of  post.comments)
-          
-          {
-            postBox = `<div class= post>
-            <div class= post-text><div class="delete" onclick="deletePost()"> x</div> ${post.text}   </div>
- 
-              
-            <div class="comments"> ${comment.text }</div>
-             
-          
-           
-            <input type='text' placeholder='Got sth to say?'  id="input-comment" >  
-            <button class='comments'> add comment</button>
-            </div>`  
-
-            $(".posts").append(postBox)
-           
-          } 
-           $(".posts").append(postBox)
-  }
-
-         /* for(let post of posts)
-         {   
-           for(let comment of  this.post.comments)
-          
-          {
-         
-          }*/
-  }
-
-
-
-
-       
-   
-          
-  
-    
- 
   return {
     renderPostsk : renderPosts,
    
   }
 
 }
-
